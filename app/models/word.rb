4ed7d39e84e9bdc42325abf9eadb52a05d3dfab5
thaotp <thaotp@nustechnology.com>
2016-07-30 17:40:10 +0900

    Update

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,7 +2,7 @@ class Word < ActiveRecord::Base
   before_create :set_romanji, :set_kanji, :set_times
   validates_presence_of :name
   scope :random, -> { order(updated_at: :asc).limit(1).first }
-  scope :fetch_quiz, -> (lesson) { select(:id, :name, :mean).where(lesson: lesson).order(updated_at: :asc).limit(9) }
+  scope :fetch_quiz, -> (lesson) { select(:id, :name, :name_jp, :mean).where(lesson: lesson).order(updated_at: :asc).limit(8) }
   def set_romanji
     self.romanji = self.name_jp.romaji
   end
