c09167000d1ec1acba46bc5f9b96b8141e9d3afa
thaotp <thaotp@nustechnology.com>
2016-07-31 23:18:18 +0900

    Update

--- app/models/word.rb
+++ app/models/word.rb
@@ -4,7 +4,7 @@ class Word < ActiveRecord::Base
   validates_presence_of :name
   scope :top_three, -> { where(lesson: Word.uniq.pluck(:lesson).max(self.max_lesson)) }
   scope :random, -> { order(updated_at: :asc).limit(1).first }
-  scope :fetch_quiz, -> { select(:id, :name, :name_jp, :mean, :kanji_note, :romanji).order(updated_at: :asc).limit(4) }
+  scope :fetch_quiz, -> { select(:id, :name, :name_jp, :mean, :kanji_note, :romanji, :kanji).order(updated_at: :asc).limit(4) }
   def set_romanji
     self.romanji = self.name_jp.romaji
   end
